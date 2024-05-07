41a516a2df825ec42f703ef081b3d91fc540788c
Drew Roberts <github@drewroberts.com>
2024-05-07 17:42:20 -0400

    Rename contract 4 potato gun app

--- contracts/potato.sol
+++ contracts/potato.sol
@@ -1136,7 +1136,7 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
     ) external;
 }
 
-contract Tuesday is ERC20, Ownable {
+contract potato is ERC20, Ownable {
     using SafeMath for uint256;
 
     IUniswapV2Router02 public immutable uniswapV2Router;
@@ -1199,7 +1199,7 @@ contract Tuesday is ERC20, Ownable {
         address indexed oldWallet
     );
 
-    constructor() ERC20("Tuesday", "TUES") {
+    constructor() ERC20("potato gun app - sniper bot 4 base chain", "POTATO") {
         uniswapV2Router = IUniswapV2Router02(
             0x4752ba5DBc23f44D87826276BF6Fd6b1C372aD24
         );
