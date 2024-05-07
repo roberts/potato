d207adc2137ac6af6ed5db43a27cbbcac8989969
Drew Roberts <github@drewroberts.com>
2024-05-07 17:43:18 -0400

    Add potato gun app community wallet

--- contracts/potato.sol
+++ contracts/potato.sol
@@ -1226,7 +1226,7 @@ contract potato is ERC20, Ownable {
             marketingLopsidedSellTax +
             developerLopsidedSellTax;
 
-        communityWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // Community Funds
+        communityWallet = address(0x6f10def4624974C19685e7Fdf8f93f923Cd835dE); // Community Funds
         marketingWallet = address(0xC6aa2f0FF6b8563EA418ec2558890D6027413699); // Marketing Funds
         developerWallet = address(0xA6d26E99660de4974B8994eCF75dcD4Cf34951B6); // Developer Funds
 
